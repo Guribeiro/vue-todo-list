7f7585e3b345e0e2972506ac49bf40c1a3ac6be7
Gustavo Henrique Ribeiro Dias <gugahribeiro@hotmail.com>
2023-06-27 11:50:14 -0300

    setup app

---
+++ src/components/Button.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+  interface Props {
+    text: string;
+  }
+  const props = defineProps<Props>()
+</script>
+
+<script lang="ts">
+  export default {
+    name: 'button-primary',
+  
+  }
+</script>
+
+<template>
+  <div>
+    <button>{{props.text }}</button>
+  </div>
+</template>
+
+<style scoped>
+  button {
+    border: none;
+    width: 100%;
+
+    background-color: #00796B;
+    padding: 12px;
+    border-radius: 4px;
+
+    color: #FFF;
+    font-size: 1rem;
+    transition: 300ms;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #00695C;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,91 @@
+<script lang="ts">
+import { reactive } from 'vue';
+import ButtonPrimary from '../components/Button.vue';
+
+interface LoginForm {
+  username: string | null;
+  password: string | null
+}
+
+export default {
+  name: 'Home-',
+  components: {
+    ButtonPrimary,
+  },
+  setup() {
+    const form = reactive<LoginForm>({
+      username: null,
+      password: null,
+    })
+
+    return form
+  },
+
+  methods: {
+    onSubmit() {
+      console.log({
+        username: this.username,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <h1>Formulário de login</h1>
+    <form @submit.prevent="onSubmit">
+      <label for="username">
+        username
+      </label>
+      <input id="username" v-model="username" type="text">
+      <label for="password">
+        password
+      </label>
+      <input id="password" v-model="password" type="password" >
+      <ButtonPrimary class="submit-button" text="Enviar" />
+    </form>
+
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 460px;
+  margin: 0 auto;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 1rem;
+  font-weight: 500;
+}
+
+
+label {
+  display: block;
+  padding: 4px 0;
+}
+
+input {
+  display: block;
+  width: 100%;
+
+  padding: 12px;
+  border-radius: 4px;
+
+  border: none;
+}
+
+input + label {
+  margin-top: .4rem;
+}
+
+.submit-button {
+  margin-top: 1rem;
+}
+</style>
